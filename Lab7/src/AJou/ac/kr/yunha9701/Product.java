8c026daec6b258eed445f844e3e689b582d9b051
Kimyunha <k9779k@naver.com>
2017-05-01 10:59:21 +0900

    create product class

---
+++ Lab7/src/AJou/ac/kr/yunha9701/Product.java
@@ -0,0 +1,13 @@
+package AJou.ac.kr.yunha9701;
+
+public abstract class Product {
+ private double price;
+
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+  public abstract String getName();
+}
