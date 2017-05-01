17f9fafb3300b39970eb70ba34601466b9d978f5
sunghee <spring@naver.com>
2017-05-01 11:47:31 +0900

    create laptop class

---
+++ Lab7/src/AJou/ac/kr/yunha9701/Laptop.java
@@ -0,0 +1,51 @@
+package AJou.ac.kr.yunha9701;
+
+public class Laptop extends Product implements Networked, DataStorage{
+	
+	private double totalCapacity;
+	private double usedCapacity;
+	
+	@Override
+	public double getFreeCapacity(){
+		return 1000000;
+	}
+	@Override
+	public void format(){
+		System.out.println("Laptop is formated :)");
+	}
+	@Override
+	public boolean isConnected(){
+		return true;
+	}
+	
+	@Override
+	public double maxSpeed(){
+		return 1000000;
+	}
+	
+	@Override
+	public String getName(){
+		String Name = "Laptop";
+		return Name; 
+	}
+	
+	public double getTotalCapacity() {
+		return totalCapacity;
+	}
+	public void setTotalCapacity(double totalCapacity) {
+	
+		if(totalCapacity>0)
+			this.totalCapacity = totalCapacity;
+	}
+	
+	
+	public double getUsedCapacity() {
+		return usedCapacity;
+	}
+	
+	public void setUsedCapacity(double usedCapacity) {
+		if(usedCapacity>0)
+			this.usedCapacity = usedCapacity;
+	}
+
+}
