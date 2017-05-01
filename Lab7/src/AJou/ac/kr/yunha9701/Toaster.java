fe958089b65492f359c16b7e6d288e8a6b820f1f
Kimyunha <k9779k@naver.com>
2017-05-01 11:43:39 +0900

    1

---
+++ Lab7/src/AJou/ac/kr/yunha9701/Toaster.java
@@ -0,0 +1,17 @@
+package AJou.ac.kr.yunha9701;
+
+public class Toaster extends Product implements Cooker{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String prepareFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
