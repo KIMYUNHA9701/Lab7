8c135ff221b9d838af64a1af29ea2067ef2ec431
Eumjihye <jihye97@ajou.ac.kr>
2017-05-01 11:44:00 +0900

    create class SmartTV;)

--- Lab7/src/AJou/ac/kr/yunha9701/SmartTV.java
+++ Lab7/src/AJou/ac/kr/yunha9701/SmartTV.java
@@ -1,5 +1,37 @@
 package AJou.ac.kr.yunha9701;
 
-public class SmartTV {
+
+public class SmartTV extends Product implements Networked{
+
+	private int channel;
+
+	public SmartTV() {
+		
+	}
+	
+	@Override
+	public boolean isConnected() {
+		return true;
+	}
+
+	@Override
+	public double maxSpeed() {
+		return 100;
+	}
+	
+	@Override
+	public String getName() {
+		return "SmartTV";
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		if (channel > 0)
+			this.channel = channel;
+	}
 
 }
+
